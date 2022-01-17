81e4be718ba3c82cc8aa908a183d03976650ebd9
username <storyno7@gmail.com>
2022-01-18 02:47:42 +0900

    🚧Banner image insertion.

---
+++ src/components/Banner.js
@@ -0,0 +1,91 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <a href="">
+            <img src="https://www.wanted.co.kr/events/livetalk28" alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img
+              src="https://static.wanted.co.kr/images/banners/1489/312a0c29.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img
+              src="https://static.wanted.co.kr/images/banners/1473/41f7b36e.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img
+              src="https://static.wanted.co.kr/images/banners/1486/fba2df30.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img
+              src="https://static.wanted.co.kr/images/banners/1487/0d36f0b5.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img
+              src="https://static.wanted.co.kr/images/banners/1460/619f3af7.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img
+              src="https://static.wanted.co.kr/images/banners/1488/baa54448.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img
+              src="https://static.wanted.co.kr/images/banners/1490/0b775035.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img
+              src="https://static.wanted.co.kr/images/banners/1468/3df61cbc.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img
+              src="https://static.wanted.co.kr/images/banners/1484/b2853456.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+      </div>
+      <button></button>
+      <button></button>
+    </div>
+  )
+}
+
+export default Banner
